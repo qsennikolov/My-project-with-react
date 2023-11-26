028d0aa4692b4efa8e3f319bbc1048d28769316e
qsennikolov <qsennikolov@abv.bg>
2023-11-26 14:28:26 +0200

    working login form
    add context

--- client/components/logIn/LogIn.jsx
+++ client/components/logIn/LogIn.jsx
@@ -1,13 +1,14 @@
+import { useContext } from 'react';
 import useForm from '../../hooks/useForm';
+import AuthContext from '../contexts/authContext.js';
 
 const LoginFormKeys = {
     Email: 'email',
     Password: 'password',
 }
 
-export default function LogIn ({
-    loginSubmitHandler,
-}) {
+export default function Login () {
+    const { loginSubmitHandler } = useContext(AuthContext)
     const {values, onChange, onSubmit} = useForm(loginSubmitHandler,{
         [LoginFormKeys.Email]:"",
         [LoginFormKeys.Password]: "",
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,43 @@
+import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
+
+import * as authService from '../components/services/authService'
+import AuthContext from '../components/contexts/authContext'
 
 import Navbar from '../components/navbar/Navbar.jsx'
 import Navi from '../components/navigation/Navi.jsx'
 import Home from "../components/home/Home.jsx"
-
 import Collections from '../components/main/Collections.jsx'
 import Search from '../components/search/Search.jsx'
 // import Appdownload from '../components/appdownload.jsx/AppDownload.jsx'
 import Blog from '../components/blog/Blog.jsx'
 import Contact from '../components/contact/Contact.jsx'
 import Footer from '../components/footer/Footer.jsx'
-import LogIn from '../components/logIn/LogIn.jsx'
+import Login from '../components/logIn/Login.jsx'
 import Register from '../components/register/Register.jsx'
-import { useState } from 'react'
 
 
 function App() {
 	const [auth, setAuth] = useState({});
 
-	const loginSubmitHandler = (values) => {
-		console.log(values);
+	const loginSubmitHandler = async (values) => {
+        const result = await authService.login(values.email, values.password);
+        console.log(result);
 	}
+
+    const registerSubmitHandler = async (values) => {
+        // console.log(values);
+    }
+
+    const values = {
+        loginSubmitHandler,
+        registerSubmitHandler,
+        username: auth.username,
+        email: auth.email,
+    }
+
 	return (
-    <>
+    <AuthContext.Provider value={{loginSubmitHandler}}>
     <Navbar />
     <Navi />
    
@@ -33,13 +48,13 @@ function App() {
             <Route path='/shop' element= { <Search />} />
             {/* <Route path='/appdownload' element= {<Appdownload />} /> */}
             <Route path='/contact' element={ <Contact />} />
-            <Route path='/login' element= { <LogIn loginSubmitHandler={loginSubmitHandler} /> } />
+            <Route path='/login' element= { <Login /> } />
             <Route path='register' element= { < Register />} />
 
         </Routes>
     <Footer />
 
-  </>
+  </AuthContext.Provider>
   )
 }
 
